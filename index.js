fee5514989486364e607477a6ad85c6dcc108e7e
ClaytonxParrish <105694697+ClaytonxParrish@users.noreply.github.com>
2023-03-01 12:14:01 -0800

    made newImage/ newItem function

--- index.js
+++ index.js
@@ -1,41 +1,32 @@
-let greenCharacter = document.createElement('img')
-greenCharacter.src = 'assets/green-character.gif'
-greenCharacter.style.position = 'fixed'
-greenCharacter.style.left = '100px'
-greenCharacter.style.bottom = '100px'
-document.body.append(greenCharacter)
+// newImage functon//
+function newImage(url,left,bottom) {
+    let object = document.createElement('img')
+    object.src = url
+    object.style.left = left + 'px'
+    object.style.bottom = bottom + 'px'
+    object.style.position = 'fixed'
+    document.body.append(object)
+    return object
+}
 
-let pineTree = document.createElement('img')
-pineTree.src = 'assets/pine-tree.png'
-pineTree.style.position = 'fixed'
-pineTree.style.left = '450px'
-pineTree.style.bottom = '200px'
-document.body.append(pineTree)
+// newItem function//
+function newItem(url, left, bottom){
+    let object = newImage(url, left, bottom)
 
-let tree = document.createElement('img')
-tree.src = 'assets/tree.png'
-tree.style.position = 'fixed'
-tree.style.left = '200px'
-tree.style.bottom = '300px'
-document.body.append(tree)
+    object.addEventListener('dblclick', () => {
+        object.remove()
+    })
+}
 
-let pillar = document.createElement('img')
-pillar.src = 'assets/pillar.png'
-pillar.style.position = 'fixed'
-pillar.style.left = '350px'
-pillar.style.bottom = '100px'
-document.body.append(pillar)
+//Images//
+newImage('assets/green-character.gif', 100, 100)
+newImage('assets/pine-tree.png', 450, 200)
+newImage('assets/tree.png', 200, 300)
+newImage('assets/pillar.png', 350, 100)
+newImage('assets/crate.png', 150, 200)
+newImage('assets/well.png', 500, 425)
 
-let crate = document.createElement('img')
-crate.src = 'assets/crate.png'
-crate.style.position = 'fixed'
-crate.style.left = '150px'
-crate.style.bottom = '200px'
-document.body.append(crate)
-
-let well = document.createElement('img')
-well.src = 'assets/well.png'
-well.style.position = 'fixed'
-well.style.left = '500px'
-well.style.bottom = '425px'
-document.body.append(well)
+//Items with Eventlistener//
+newItem('assets/sword.png', 500, 405)
+newItem('assets/sheild.png', 165, 185)
+newItem('assets/staff.png', 600, 100)
